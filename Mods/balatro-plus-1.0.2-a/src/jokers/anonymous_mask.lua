6734f3b120804c664cbfb153894d1f6ee03a0d9e
mckellar11 <128339167+mckellar11@users.noreply.github.com>
2025-02-18 23:11:34 -0500

    Add files via upload

---
+++ Mods/balatro-plus-1.0.2-a/src/jokers/anonymous_mask.lua
@@ -0,0 +1,16 @@
+local j = {
+  loc_txt = {
+    name = "Anonymous Mask",
+    text = {
+      "All {C:attention}face cards{}",
+      "can't be {C:red}debuffed{}",
+    },
+  },
+  rarity = 2,
+  cost = 8,
+  atlas = 17,
+
+  blueprint_compat = false,
+}
+
+return j
---
+++ Mods/balatro-plus-1.0.2-a/src/jokers/not_found.lua
@@ -0,0 +1,15 @@
+local j = {
+  loc_txt = {
+    name = "Joker Not Found",
+    text = {
+      "{C:blue}Common{} Jokers no longer",
+      "{C:attention}appear{} in the {C:attention}shop",
+    },
+  },
+  atlas = 42,
+  rarity = 2,
+
+  blueprint_compat = false,
+}
+
+return j
