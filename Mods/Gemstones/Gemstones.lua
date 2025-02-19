8bf58abde4ac741e6ef57f4e9e48905e9cd69b6c
mckellar11 <128339167+mckellar11@users.noreply.github.com>
2025-02-18 22:35:38 -0500

    more gemstones files

---
+++ Mods/Gemstones/Gemstones.lua
@@ -0,0 +1,72 @@
+-- Defining Globals
+Gemstones = SMODS.current_mod
+Gemstones_Config = Gemstones.config
+
+-- Undiscovered Sprite
+SMODS.UndiscoveredSprite{
+    key = 'Gemstone',
+    atlas = 'gems',
+    pos = { x = 9, y = 4 },
+}
+
+-- Mod Icon
+SMODS.Atlas{
+	key = "modicon",
+	path = "modicon.png",
+	px = 32,
+	py = 32
+}
+
+-- Create Classes for easier content production
+Gemstones.GemSlot = SMODS.Sticker:extend{
+	rate = 0.0,
+	order = 100,
+	should_apply = false,
+    prefix_config = { key = false },
+    joker_compat = true,
+    card_compat = true,
+
+	draw = function(self, card) --don't draw shine
+		G.shared_stickers[self.key].role.draw_major = card
+		G.shared_stickers[self.key]:draw_shader("dissolve", nil, nil, nil, card.children.center)
+	end,
+    added = function(self, card) end,
+    removed = function(self, card) end,
+}
+
+Gemstones.GemstoneConsumable = SMODS.Consumable:extend{
+	set = "Gemstone",
+	discovered = false,
+	should_apply = false,
+	cost = 3,
+	order = 100,
+	config = { max_highlighted = 1, sticker_id = "gemslot_empty" },
+
+    loc_vars = function(self, info_queue)
+        info_queue[#info_queue + 1] = { key = self.config.sticker_id, set = "Other", vars = {} }
+        return { vars = { self.config.max_highlighted } }
+    end,
+	can_use = function(self, card) return Gemstones.can_use_gemstone_consumeable(self, card) end,
+    use = function(self, card, area, copier) Gemstones.use_gemstone_consumeable(self, card, area, copier) end,
+}
+
+-- Load Item Files
+local files = NFS.getDirectoryItems(SMODS.current_mod.path .. "content")
+for _, file in ipairs(files) do
+	print("Gemstones | Loading Item file " .. file)
+	local f, err = SMODS.load_file("content/" .. file)
+	if err then
+		error(err)
+	end
+	f()
+end
+
+-- Gather Gem Slot Pools
+Gemstones.pools = { both = {}, cards = {}, jokers = {} }
+for k, v in pairs(SMODS.Stickers) do
+	if v.key:find("gemslot") then
+		if v.joker_compat then table.insert(Gemstones.pools.jokers, v.key) end
+		if v.card_compat then table.insert(Gemstones.pools.cards, v.key) end
+		if v.joker_compat and v.card_compat then table.insert(Gemstones.pools.both, v.key) end
+	end
+end
---
+++ Mods/Gemstones/README.md
@@ -0,0 +1,10 @@
+# Gemstones
+Gemstones is a Balatro mod that adds a new unique card modifier: **Gem Slots**! Gem Slots can be obtained in multiple ways, and can hold Gemstones.
+
+![image](https://github.com/0fficialHalo/Gemstones/blob/main/assets/GitHub/GemSlots_Preview.png?raw=true)
+
+## How to install?
+1. Click [here](www.github.com/0fficialHalo/Gemstones/releases/latest) to go to the latest release
+2. Scroll down and click on the .zip file to download the release
+3. Extract the `Gemstones` and `Talisman` folder in the downloaded file into your Mods folder (`%AppData%/Roaming/Balatro/Mods` for Windows)
+4. Done! If you have any problems, feel free to ask for help in the server!
